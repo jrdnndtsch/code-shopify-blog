9e36c25b771dde90c7a1033d7028835dc50fd261
deartrudence <trudy.macnabb@gmail.com>
2016-02-21 10:16:11 -0500

    restrict registrations

--- app/controllers/my_devise/registrations_controller.rb
+++ app/controllers/my_devise/registrations_controller.rb
@@ -1,5 +1,9 @@
 class MyDevise::RegistrationsController < Devise::RegistrationsController
 
+    def new
+        flash[:info] = 'Registrations are not open.'
+        redirect_to root_path
+    end
 
     def after_sign_up_path_for(resource)
       new_profile_path
