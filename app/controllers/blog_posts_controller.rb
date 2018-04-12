1cf928f4a5a1fdfffc88ea02971f61f78ebda544
jrdnndtsch <jordanbdeutsch@gmail.com>
2018-04-12 13:52:16 -0400

    style updates

--- app/controllers/blog_posts_controller.rb
+++ app/controllers/blog_posts_controller.rb
@@ -16,6 +16,9 @@ class BlogPostsController < ApplicationController
     else  
       @blog_posts = BlogPost.published?.order(updated_at: :desc).paginate(:page => params[:page], :per_page => 9)
     end
+    @theme_posts = BlogPost.published?.by_category('Theme').order(:road_map_order)
+    @app_posts = BlogPost.published?.by_category('App').order(:road_map_order)
+    @js_posts = BlogPost.published?.by_category('JSSDK').order(:road_map_order)
     # @authors = User.all.includes(:blog_posts)
     # @tags = ActsAsTaggableOn::Tag.all.where.not(taggings_count: 0).order(taggings_count: :desc)
     # fresh_when etag: @blog_posts, last_modified: @blog_posts.maximum(:updated_at), public: true
